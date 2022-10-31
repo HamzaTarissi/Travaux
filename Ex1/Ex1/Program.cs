540c9639a568b4b14375def6ae5c28a66830e38f
HamzaTarissi <hamzatarissi@gmail.com>
2022-10-31 10:57:16 +0100

    Ex1solution1

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i,isfirst;
+            Console.WriteLine("veuillez entrer un nombre");
+            n = Console.Read();
+            isfirst = 1;
+            for ( i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                    isfirst = 0;
+                Console.WriteLine("ce nombre n'est pas premier ");
+                break;
+            }
+                if (isfirst == 1)
+                    Console.WriteLine("{0} est un nombre premier",n);
+        } 
+    }
+}
---
+++ ex1solution2/ex1solution2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1solution2
+{
+    internal class Program
+    {
+            public static bool IsPrime(int nb)
+            {
+                if (nb < 0)
+                    return IsPrime(-nb);
+                else if (nb <= 3)
+                    return true;
+                else
+                {
+                    int sqrt_int = (int)Math.Sqrt(nb);
+                    for (int i = 2; i <= sqrt_int; i++)
+                    {
+                        if (nb % i == 0)
+                            return false;
+                    }
+
+                    return true;
+                }
+            }
+        }
+    }
+}
