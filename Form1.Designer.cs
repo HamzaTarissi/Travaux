3793709e4c7b78c47e4c9465a5c3d6cd4710bd87
HamzaTarissi <116294519+HamzaTarissi@users.noreply.github.com>
2022-12-02 01:28:37 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ExamQcm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textQuestion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textOption1 = new System.Windows.Forms.TextBox();
+            this.textOption2 = new System.Windows.Forms.TextBox();
+            this.textOption3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textAnswer = new System.Windows.Forms.TextBox();
+            this.buttnext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add questions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Set Question";
+            // 
+            // textQuestion
+            // 
+            this.textQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textQuestion.Location = new System.Drawing.Point(172, 116);
+            this.textQuestion.Name = "textQuestion";
+            this.textQuestion.Size = new System.Drawing.Size(311, 41);
+            this.textQuestion.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Option 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Option 2 ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(46, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Option 3";
+            // 
+            // textOption1
+            // 
+            this.textOption1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOption1.Location = new System.Drawing.Point(172, 229);
+            this.textOption1.Name = "textOption1";
+            this.textOption1.Size = new System.Drawing.Size(311, 41);
+            this.textOption1.TabIndex = 6;
+            // 
+            // textOption2
+            // 
+            this.textOption2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOption2.Location = new System.Drawing.Point(172, 276);
+            this.textOption2.Name = "textOption2";
+            this.textOption2.Size = new System.Drawing.Size(311, 41);
+            this.textOption2.TabIndex = 7;
+            // 
+            // textOption3
+            // 
+            this.textOption3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOption3.Location = new System.Drawing.Point(172, 323);
+            this.textOption3.Name = "textOption3";
+            this.textOption3.Size = new System.Drawing.Size(311, 41);
+            this.textOption3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(562, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Answer";
+            // 
+            // textAnswer
+            // 
+            this.textAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAnswer.Location = new System.Drawing.Point(567, 297);
+            this.textAnswer.Name = "textAnswer";
+            this.textAnswer.Size = new System.Drawing.Size(311, 41);
+            this.textAnswer.TabIndex = 10;
+            // 
+            // buttnext
+            // 
+            this.buttnext.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttnext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttnext.Location = new System.Drawing.Point(653, 366);
+            this.buttnext.Name = "buttnext";
+            this.buttnext.Size = new System.Drawing.Size(125, 62);
+            this.buttnext.TabIndex = 11;
+            this.buttnext.Text = "Next";
+            this.buttnext.UseVisualStyleBackColor = false;
+            this.buttnext.Click += new System.EventHandler(this.buttnext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(890, 510);
+            this.Controls.Add(this.buttnext);
+            this.Controls.Add(this.textAnswer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textOption3);
+            this.Controls.Add(this.textOption2);
+            this.Controls.Add(this.textOption1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textQuestion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(908, 557);
+            this.MinimumSize = new System.Drawing.Size(908, 557);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textQuestion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textOption1;
+        private System.Windows.Forms.TextBox textOption2;
+        private System.Windows.Forms.TextBox textOption3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textAnswer;
+        private System.Windows.Forms.Button buttnext;
+    }
+}
+
